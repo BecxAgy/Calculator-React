77640b8200d9c262303897edf50f26efaadc23f1
Rebeca Vitória de Aguiar Coutinho <rebeca.coutinho@kempetro.com.br>
2023-07-11 10:34:30 -0300

    Finish project

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -5,100 +5,165 @@ import './Calculator.css'
 
 function Calculator() {
 
-const number = useState(0);
+const [number, setNumber] = useState(0);
+const [operator, setOperator] = useState();
+const [visor, setvisor] = useState(0)
+const [oldnum, setOldNum] = useState();
+
+function inputNum(e){
+  var input = e.target.value;
+
+  if(number === 0){
+    setvisor( input);
+    setNumber(input);
+  }else{
+
+    setNumber(number + input);
+    setvisor(visor+input)
+  }
+}
+
+//clear
+function clearAll(){
+  setNumber(0);
+  setvisor(0);
+}
+//porcentage
+function porcentage(){
+
+  var input = number/100;
+  setNumber(input);
+  setvisor(input);
+}
+
+//toggleSinal(Trocar sinal)
+function toggleSign(){
+  if(number > 0){
+    setNumber(-number);
+    setvisor(-number);
+  }else{
+    setNumber(Math.abs(number));
+    setvisor(Math.abs(number));
+  }
+}
+
+//operatorHandler
+function operatorHandler(e){
+  var operatorInput = e.target.value;
+  setOperator(operatorInput);
+  setvisor(number + operatorInput);
+  setOldNum(number);
+  setNumber(0);
+  
+}
+
+//calculate
+function calculate(){
+  if(operator === "/"){
+    setvisor(oldnum/number);
+  }
+  else if(operator === "x"){
+    setvisor(oldnum*number);
+  }
+  else if(operator === "-"){
+    setvisor(oldnum-number);
+  }
+  else if(operator === "+"){
+    setvisor(oldnum + number);
+  }
+  
+}
   return (
     <Box m={20}>
-      
-           
          <Container maxWidth='xs' >
 
             <div className='wrapper'>
+                <Box m={8}/>
+                    <h1 className='result'>{visor}</h1>
+                    
+                    <button onClick={clearAll} className='white'>
+                    AC
+                    </button>
 
-                <h1>{number}</h1>
-                
-                <button className='white'>
-                AC
-                </button>
+                    <button onClick={toggleSign} className='white'>
+                    +/-
+                    </button>
 
-                <button className='white'>
-                +/-
-                </button>
+                    <button onClick={porcentage} className='white'>
+                    %
+                    </button>
 
-                <button  className='white'>
-                %
-                </button>
+                    <button onClick={operatorHandler}  value="/" className='orange'>
+                    /
+                    </button>
 
-                <button className='orange'>
-                /
-                </button>
+                    <button  className='grey' onClick={inputNum} value={7}> 
+                    7
+                    </button>
+                    
+                    <button  className='grey' onClick={inputNum} value={8}>
+                    8
+                    </button>
 
-                <button  className='grey'>
-                7
-                </button>
-                
-                <button  className='grey'>
-                8
-                </button>
+                    <button  className='grey' onClick={inputNum} value={9}>
+                    9
+                    </button>
 
-                <button  className='grey'>
-                9
-                </button>
+                    <button onClick={operatorHandler}  value="x" className='orange' >
+                    x
+                    </button>
 
-                <button  className='orange'>
-                x
-                </button>
+                    <button  className='grey' onClick={inputNum} value={4}>
+                    4
+                    </button>
 
-                <button  className='grey'>
-                4
-                </button>
+                    <button  className='grey' onClick={inputNum} value={5}>
+                    5
+                    </button>
 
-                <button  className='grey'>
-                5
-                </button>
+                    <button  className='grey' onClick={inputNum} value={6}>
+                    6
+                    </button>
 
-                <button  className='grey'>
-                6
-                </button>
+                    <button onClick={operatorHandler}  value="-" className='orange' >
+                    -
+                    </button>
 
-                <button  className='orange'>
-                -
-                </button>
+                    <button  className='grey' onClick={inputNum} value={1}>
+                    1
+                    </button>
 
-                <button  className='grey'>
-                1
-                </button>
+                    <button  className='grey' onClick={inputNum} value={2}>
+                    2
+                    </button>
 
-                <button  className='grey'>
-                2
-                </button>
+                    <button  className='grey' onClick={inputNum} value={3}>
+                    3
+                    </button>
 
-                <button  className='grey'>
-                3
-                </button>
+                    <button onClick={operatorHandler}  value="+" className='orange' >
+                    +
+                    </button>
 
-                <button  className='orange'>
-                +
-                </button>
+                    <button  className='grey' onClick={inputNum} value={0}>
+                    0
+                    </button>
 
-                <button  className='grey'>
-                0
-                </button>
+                    <button  className='grey'>
+                    ,
+                    </button>
 
-                <button  className='grey'>
-                ,
-                </button >
+                    <button  className='black'>
+                    .
+                    </button>
 
-                <button  className='black'>
-                
-                </button >
-
-                <button  className='orange'>
-                =
-                </button>
+                    <button onClick={calculate} className='orange'>
+                    =
+                    </button>
+                  
             </div>
              
-        </Container>
-            
+        </Container>     
    
     </Box>
        
